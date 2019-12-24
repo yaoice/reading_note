70d175f7de18c65f6e8ce4c43d03acc7edc280f1
xiabingyao <xiabingyao@tencent.com>
2019-12-24 20:49:53 +0800

    add controller goroutine numbers

---
+++ 控制goroutine并发数量/main.go
@@ -0,0 +1,58 @@
+/*
+通过sync.WaitGroup + channel控制数量
+参考k8s.io/client-go/util/workqueue/parallelizer.go
+*/
+
+package main
+
+import (
+	"context"
+	"sync"
+)
+
+type DoWorkPieceFunc func(piece int)
+
+// Parallelize is a very simple framework that allows for parallelizing
+// N independent pieces of work.
+//
+// Deprecated: Use ParallelizeUntil instead.
+func Parallelize(workers, pieces int, doWorkPiece DoWorkPieceFunc) {
+	ParallelizeUntil(nil, workers, pieces, doWorkPiece)
+}
+
+// ParallelizeUntil is a framework that allows for parallelizing N
+// independent pieces of work until done or the context is canceled.
+func ParallelizeUntil(ctx context.Context, workers, pieces int, doWorkPiece DoWorkPieceFunc) {
+	var stop <-chan struct{}
+	if ctx != nil {
+		stop = ctx.Done()
+	}
+
+	toProcess := make(chan int, pieces)
+	for i := 0; i < pieces; i++ {
+		toProcess <- i
+	}
+	close(toProcess)
+
+	if pieces < workers {
+		workers = pieces
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(workers)
+	for i := 0; i < workers; i++ {
+		go func() {
+			defer utilruntime.HandleCrash()
+			defer wg.Done()
+			for piece := range toProcess {
+				select {
+				case <-stop:
+					return
+				default:
+					doWorkPiece(piece)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
